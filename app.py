ca376e2f64c306d297327cc0936ad47e36bade66
pikvic <truepikvic@gmail.com>
2021-04-22 14:50:07 +1000

    Captcha

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ import json
 from PIL import Image, ImageOps
 from tensorflow import keras
 import numpy as np
+import uuid
+import random
 
+model = keras.models.load_model('model')
 
+sessions = {}
 
 def to_grayscale(img):
     image = Image.open(img)
@@ -16,11 +20,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', text="This is My!!! Text")
+    session_id = str(uuid.uuid4())
+    sessions[session_id] = random.choice(list(range(10)))
+    return render_template('index.html', session=session_id, number=sessions[session_id])
 
 @app.route('/upload', methods=['post'])
 def upload():
-    model = keras.models.load_model('model')
+    print(sessions)
+    session_id = request.headers.get("session")
     with open("temp.png","wb") as f:
         data = json.loads(request.data)['image']
         data = data.replace("data:image/png;base64,", '')
@@ -32,4 +39,15 @@ def upload():
     np_frame = np.expand_dims(np_frame, 0)
 
     result = model.predict(np_frame)
-    return make_response(jsonify({"success": "true", "digit": str(np.argmax(result[0])), "result": str(result[0])}), 200)
+    digit = np.argmax(result[0])
+    number = sessions[session_id]
+    res = {
+        "digit": digit,
+        "number": number,
+        "result": str(result)
+    }
+    if digit == number:
+        res["success": True]
+    else:
+        res["success": False]
+    return make_response(jsonify(res), 200)
